34e1020a9ffdd89f3a669d7030f247f0ec479f4b
AnDeRR <anderr225@gmail.com>
2019-10-28 01:16:31 +0700

    add memoization

--- Clojure/lab2/src/lab2/core.clj
+++ Clojure/lab2/src/lab2/core.clj
@@ -2,31 +2,31 @@
 
 (defn get_trapezoid_area
   [f l r]
-  (* (- r l)(/ (+ (f l) (f r)) 2))  
-  )
+  (* (- r l)(/ (+ (f l) (f r)) 2)))
 
-(defn inner_integral
-  [f l r h]
-  (if
-    (>= l r)
-    0
-    (+ 
-      (get_trapezoid_area f l (+ l h))
-      (inner_integral f (+ l h) r h)))  
-  )
+(defn inner_memoized_integral
+  []
+  (let 
+    [inner_integral
+     (fn [mem_integral f l r h]
+       (let [inner (fn [f l r h] (mem_integral mem_integral f l r h))] 
+         (if
+           (>= l r)
+           0
+           (+ 
+             (get_trapezoid_area f l (+ l h))
+             (inner f (+ l h) r h)))))
+     mem_integral (memoize inner_integral)]
+    (partial mem_integral mem_integral)))
 
-(defn calculate_integral
+(defn calculate_mem_integral
   [f x]
   (if (>= x 0) 
     (
-      let [h 0.1 border (* h (int (/ x h)))] 
-      (+ (inner_integral f 0 border h) 
+      let [h 0.05 border (* h (int (/ x h)))] 
+      (+ ((inner_memoized_integral) f 0 border h) 
          (get_trapezoid_area f border x))
       )
-    (throw (IllegalArgumentException. "x should be > 0")))
-  )
+    (throw (IllegalArgumentException. "x should be > 0"))))
 
-(defn get_integration_fn [f]
-  (fn [x] (calculate_integral f x)))
-
-(calculate_integral (fn [x] (* x x)) 5)
+(calculate_mem_integral (fn [x] (* x x)) 2)
