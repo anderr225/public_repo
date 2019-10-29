a875bea97b66c7e91688e3712b03bd259285a57d
AnDeRR <anderr225@gmail.com>
2019-10-29 23:42:05 +0700

    add tests

--- Clojure/lab2/test/lab2/core_test.clj
+++ Clojure/lab2/test/lab2/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [lab2.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest get_trapezoid_area_test
+  (testing
+    (is (= 6 (get_trapezoid_area (fn [x] x) 2 4)))))
+
+(deftest get_integral_fn_test
+  (testing
+    (is (= 50 (int ((get_integral_fn (fn [x] x)) 10))))))
+
+(deftest calculate_mem_integral_test
+  (testing
+    (is (= 2 (int (calculate_mem_integral (fn [x] x) 2))))))
+
+(deftest inner_memoized_integral_test
+  (testing
+    (is (= 2 (int ((inner_memoized_integral) (fn [x] x) 0 2 0.05))))))
