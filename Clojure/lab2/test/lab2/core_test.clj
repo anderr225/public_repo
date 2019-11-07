b793d943089c2f9cfbf740294192be3c709ad5aa
anderr225 <anderr225@gmail.com>
2019-11-07 17:22:57 +0700

    add final test

--- Clojure/lab2/test/lab2/core_test.clj
+++ Clojure/lab2/test/lab2/core_test.clj
@@ -10,3 +10,16 @@
 (deftest make_integral_test
   (testing
     (is (= 333 (int ((make_integral (fn [x] (* x x))) 10))))))
+
+(defn close_enough [v1 v2] (
+  < (max (- v1 v2) (- v2 v1)) 0.3
+))
+
+(defn calc_fn [x] (/ (* x x x) 3))
+
+(deftest compare_integral_to_calculated_fn
+  (testing
+    (is (close_enough (calc_fn 1) (int ((make_integral (fn [x] (* x x))) 1))))
+    (is (close_enough (calc_fn 0.5) (int ((make_integral (fn [x] (* x x))) 0.5))))
+    (is (close_enough (calc_fn 0.3) (int ((make_integral (fn [x] (* x x))) 0.3))))))
+
